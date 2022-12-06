74e8431deb4c4f6078155e3a319961fe49ba30d8
k-keyboard <63134404@g.cmru.ac.th>
2022-12-06 15:01:55 +0700

    first commit

---
+++ src/components/core/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined
+          v-else
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from "@ant-design/icons-vue";
+
+export default defineComponent({
+    components:{
+        UserOutlined,
+        VideoCameraOutlined,
+        UploadOutlined,
+        MenuUnfoldOutlined,
+        MenuFoldOutlined,
+    },
+    setup() {
+        const collapsed = ref<boolean>(false)
+        return{}
+    },
+})
+</script>
+<style>
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+</style>
